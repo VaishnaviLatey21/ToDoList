f50237bce668bf2ac9f0282b719f4ea869313c8e
vaishnavi <87173081+VaishnaviLatey21@users.noreply.github.com>
2023-02-20 21:54:09 +0530

    checkBox

--- scripts/index.js
+++ scripts/index.js
@@ -5,21 +5,28 @@ const addTaskBtn = document.querySelector(".btn_add");
 const newtask = document.querySelector("#newtask");
 const priority = document.querySelector("#priority");
 const progressStatus = document.querySelector("#progressStatus");
-const closeBtn = document.querySelector(".btn_close")
+const closeBtn = document.querySelector(".btn_close");
+const checkBox = document.querySelector("#checkbox");
 
 function addTask() {
     var task = document.createElement('div');
+    var box_container = document.createElement('div');
+    var checkBox = document.createElement('input');
     var taskContent = document.createElement('div');
     var priorityDiv = document.createElement('div');
     var inProgress = document.createElement('div');
     var btn_container = document.createElement('div');
     var close_btn = document.createElement('button');
+    // checkBox.checked = true; 
     taskContent.innerText = newtask.value;
     priorityDiv.innerText = priority.value;
     inProgress.innerText = progressStatus.value;
     close_btn.innerText = 'x';
     close_btn.style.display = 'inline';
+    checkBox.style.display = 'block';
     btn_container.appendChild(close_btn);
+    box_container.appendChild(checkBox);
+    task.append(box_container);
     task.append(taskContent);
     task.append(priorityDiv);
     task.append(inProgress);
@@ -30,8 +37,8 @@ function addTask() {
         element.classList.add("grid-item1")
     });
     taskcontainer.appendChild(task);
-}
 
+}
 
 addTaskBtn.addEventListener("click", function () {
     if (newtask.value == '' || priority.value == '' || progressStatus.value == '') {
@@ -42,8 +49,14 @@ addTaskBtn.addEventListener("click", function () {
 })
 
 closeBtn.addEventListener("click", function() {
-    taskcontainer.removeChild(taskList);
+    // taskcontainer.removeChild(taskList);
     // taskList.remove(taskcontainer);
+    var current_tasks = document.querySelectorAll(".btn_close");
+    for (var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+        taskList.remove();
+        }
+            }
 })
 
 
