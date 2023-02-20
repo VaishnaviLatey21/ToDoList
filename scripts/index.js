be8bf3cfa400f1faa70df72a0cbb71a26aa55a82
vaishnavi <87173081+VaishnaviLatey21@users.noreply.github.com>
2023-02-20 15:03:56 +0530

    close_btn

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,33 @@ const addTaskBtn = document.querySelector(".btn_add");
 const newtask = document.querySelector("#newtask");
 const priority = document.querySelector("#priority");
 const progressStatus = document.querySelector("#progressStatus");
+const closeBtn = document.querySelector("#btn_close")
 
 function addTask() {
     var task = document.createElement('div');
     var taskContent = document.createElement('div');
     var priorityDiv = document.createElement('div');
     var inProgress = document.createElement('div');
+    var btn_container = document.createElement('div');
+    var close_btn = document.createElement('button');
     taskContent.innerText = newtask.value;
     priorityDiv.innerText = priority.value;
     inProgress.innerText = progressStatus.value;
+    close_btn.innerText = 'x';
+    close_btn.style.display = 'inline';
+    btn_container.appendChild(close_btn);
     task.append(taskContent);
     task.append(priorityDiv);
     task.append(inProgress);
+    task.append(btn_container);
     task.classList.add("grid-container");
     var taskChild = Array.from(task.childNodes);
     taskChild.forEach(element => {
         element.classList.add("grid-item1")
     });
+    // let button = document.createElement('button');
+    // button.classList.
+    // task.append();
     taskcontainer.appendChild(task);
 }
 
@@ -31,6 +41,9 @@ addTaskBtn.addEventListener("click", function () {
         addTask();
     }
 })
+
+
+
 
 
 
